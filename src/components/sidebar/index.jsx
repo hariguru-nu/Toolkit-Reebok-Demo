ae4e83c3a05675e8e2a4cdfcccc50b6db0d0a945
Hariguru <hariguru.balan@nulogic.io>
2023-06-27 12:23:41 +0530

    sparc homepage updated

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -7,12 +7,14 @@ import Transactions from "../../assets/images/transactions.svg";
 import Performance from "../../assets/images/performance.svg";
 import News from "../../assets/images/news.svg";
 import Settings from "../../assets/images/settings.svg";
-import ReebokIcon from "../../assets/images/reebok-logo.png"
+import ReebokIcon from "../../assets/images/reebok-logo.png";
 import Support from "../../assets/images/support.svg";
+import PlusIcon from "../../assets/images/plus-icon.svg";
 import styles from "./styles.module.scss";
+import Submenu from "./Submenu";
 
 const Sidebar = () => {
-  const { asPath }  = useRouter();
+  const { asPath } = useRouter();
   console.log(asPath);
 
   const [closeMenu, setCloseMenu] = useState(false);
@@ -23,9 +25,8 @@ const Sidebar = () => {
   };
 
   const handleActiveTab = (id) => {
-    console.log(id, "id");
     setActiveTab(id);
-  }
+  };
 
   const menuLinks = [
     {
@@ -54,8 +55,8 @@ const Sidebar = () => {
         {
           menuText: "OUTLET Report",
           pathname: "/report/report-outlet",
-        }
-      ]
+        },
+      ],
     },
     {
       imageSrc: Settings,
@@ -70,7 +71,7 @@ const Sidebar = () => {
           menuText: "Order Demand",
           pathname: "/orders/order-demand",
         },
-      ]
+      ],
     },
     {
       imageSrc: Support,
@@ -84,68 +85,73 @@ const Sidebar = () => {
         {
           menuText: "API",
           pathname: "/aws/api-action",
-        }
-        
-      ]
+        },
+      ],
     },
     {
       imageSrc: Performance,
       menuText: "Performance",
       pathname: "/performance",
     },
-    
-    
   ];
 
   return (
-      <div className={`${styles.sidebar} ${closeMenu === false ? styles.sidebar : styles.active}`}>
+    <div
+      className={`${styles.sidebar} ${
+        closeMenu === false ? styles.sidebar : styles.active
+      }`}
+    >
+      <div
+        className={`${styles.logoContainer} ${
+          closeMenu === false ? styles.logoContainer : styles.active
+        }`}
+      >
+        <Image src={ReebokIcon} alt="icon" className={styles.logo} />
+        <h2 className={styles.title}>REEBOK </h2>
+      </div>
+      <div
+        className={`${styles.burgerContainer} ${
+          closeMenu === false ? styles.burgerContainer : styles.active
+        }`}
+      >
         <div
-          className={`${styles.logoContainer} ${ closeMenu === false ? styles.logoContainer : styles.active}`}
-        >
-          <Image src={ReebokIcon} alt="icon" className={styles.logo} />
-          <h2 className={styles.title}>REEBOK </h2>
-        </div>
-        <div
-          className={`${styles.burgerContainer} ${closeMenu === false ? styles.burgerContainer : styles.active }`}
-        >
-          <div
-            className={styles.burgerTrigger}
-            onClick={() => {
-              handleCloseMenu();
-            }}
-          ></div>
-          <div className={styles.burgerMenu}></div>
-        </div>
-        <div
-          className={`${styles.contentsContainer} ${ closeMenu === false
-            ? styles.contentsContainer
-            : styles.active }`}
-        >
-          <ul>
-            {menuLinks?.map((links, index) => (
-              <>
-              <li key={index} 
+          className={styles.burgerTrigger}
+          onClick={() => {
+            handleCloseMenu();
+          }}
+        ></div>
+        <div className={styles.burgerMenu}></div>
+      </div>
+      <div
+        className={`${styles.contentsContainer} ${
+          closeMenu === false ? styles.contentsContainer : styles.active
+        }`}
+      >
+        <ul>
+          {menuLinks?.map((links, index) => (
+            <>
+              <li
+                key={index}
                 className={asPath === links?.pathname ? "menuActive" : ""}
                 onClick={() => handleActiveTab(index)}
               >
                 <Image src={links?.imageSrc} alt="dashboard" />
                 <Link href={links?.pathname}>{links?.menuText}</Link>
               </li>
-              
-              {/* {index === activeTab && links?.subMenu && (
+
+              {/* {index === activeTab &&
+                links?.subMenu &&
                 links?.subMenu?.map((item, index) => (
                   <li key={index} className={styles.subMenu}>
                     <Link href={item?.pathname}>{item?.menuText}</Link>
                   </li>
                 ))
-                  
-              )} */}
-              </>
-            ))}
-           
-          </ul>
-        </div>
+              } */}
+            </>
+          ))}
+        </ul>
       </div>
+    </div>
   );
 };
 
