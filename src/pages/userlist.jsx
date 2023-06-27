5f115d0c57fdfe5d1898fc435b50d03f63591f93
Hariguru <hariguru.balan@nulogic.io>
2023-06-27 16:37:17 +0530

    modified homepage content

--- src/pages/userlist.jsx
+++ src/pages/userlist.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from '@/components/layout';
 
+
 const userlist = () => {
   return (
    <Layout>
-        <h3>Welcome to Reebok Toolkit</h3>
+        <h3 style={{display: "flex", justifyContent: "center", fontSize: "40px"}}>Welcome to Reebok Toolkit</h3>
    </Layout>
   )
 }
