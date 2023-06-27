0d5afcb5c97329c7edede7ff238bfca8b784f76b
Hariguru <hariguru.balan@nulogic.io>
2023-06-27 15:40:43 +0530

    added logout option

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React,{useState} from 'react';
+ 
 import {
   BsPatchExclamation,
   BsTag,
   BsFillPersonLinesFill
 } from "react-icons/bs";
 import styles from './styles.module.scss';
+import Link from 'next/link';
 
 const Header = () => {
+  const [card,setCard]=useState(false);
+const handleClick=()=>{
+  setCard(!card)
+}
+  const renderComponent=()=>{return(<div className={styles.card}>
+      <ul className={styles.list}>
+        <li><Link href="/" className={styles.logout}>Logout </Link> </li>
+      </ul>
+    </div>
+  
+  )}
   return (
     <div className={styles.header}>
-      <div>
         <BsPatchExclamation className={styles.headerIcons}/>
         <BsTag className={styles.headerIcons}/>
-        <BsFillPersonLinesFill className={styles.headerIcons}/>
-      </div>
+         
+        <BsFillPersonLinesFill className={styles.headerIcons} onClick={handleClick} />
+        {card && renderComponent()}
+    
     </div>
   )
 }
---
+++ src/pages/performance.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "@/components/layout";
+import Table from "@/components/table";
+
+const Performance = () => {
+    return (
+       <Layout>
+            <Table />
+       </Layout>
+    );
+};
+
+export default Performance;
